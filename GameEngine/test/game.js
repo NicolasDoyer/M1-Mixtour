a90081cd523d3be79f58f7a22b3694759b120a61
Unknown <hugo.doyer97@gmail.com>
2019-01-08 14:08:48 +0100

    correction test

--- GameEngine/test/game.js
+++ GameEngine/test/game.js
@@ -5,50 +5,57 @@ import { Piece } from '../src/Piece';
 describe('Game engine', () => {
 
     // Game test
-    const game = new Game();
     it('should create', () => {
+        const game = new Game();
         assert(game);
     })
     it('should have an empty board', () => {
+        const game = new Game();
         assert(game.board);
         assert(game.board.cells.every(row => row.every(cell => cell === '')));
     });
 
-    const players = Object.values(game.players);
     it('should have 2 players red and white', () => {
+        const game = new Game();
+        const players = Object.values(game.players);
         assert(players.find(p => p.color === 'RED'));
         assert(players.find(p => p.color === 'WHITE'));
     })
     it('white player should start', () => {
+        const game = new Game();
         assert(game.turn === Piece.WHITE);
     })
     it('put pieces', () => {
+        const game = new Game();
         game.board.cells[0][0] = '';
-        game.play(game.players['W'], {type: 'put', coords: [0, 0]})
+        game.play(game.players['W'], {type: 'put', coords: [0, 0]});
         assert(game.board.cells[0][0] === 'W');
     })
     it('red player should continue', () => {
+        const game = new Game();
+        game.play(game.players['W'], {type: 'put', coords: [0, 0]});
         assert(game.turn === Piece.RED);
     })
     it('check put place already taken', () => {
+        const game = new Game();
+        game.board.cells[0][0] = 'R'
         assert.throws(() => {
             game.play(game.players['W'], {type: 'put', coords: [0, 0]})
         }, {name: 'Error', message: 'Place already taken'});
     })
-    game.players['W'].backpieces = 0;
-    game.players['R'].backpieces = 0;
     it('check pieces left', () => {
+        const game = new Game();
+        game.players['W'].backpieces = 0;
         assert.throws(() => {
             game.play(game.players['W'], {type: 'put', coords: [1,1]})
         }, { name: 'Error', message: 'Player has no pieces' });
     })
-    game.players['W'].backpieces = 15;
-    game.players['R'].backpieces = 15;
 
 //add move start or end empty OR outOfTheBoard
 
     // Move 's mechaniks
     it('check correct nb of pieces', () => {
+        const game = new Game();
         game.board.cells[0][0] = 'RR';
         game.board.cells[0][2] = 'WW';
         assert.throws(() => {
@@ -56,6 +63,7 @@ describe('Game engine', () => {
         }, { name: 'Error', message: 'Wrong number of pieces' });
     })
     it('must fit target height and distance', () => {
+        const game = new Game();
         game.board.cells[1][1] = 'WWR';
         game.board.cells[4][4] = 'WR';
         assert.throws(() => {
@@ -77,6 +85,7 @@ describe('Game engine', () => {
 
     })
     it('can split', () => {
+        const game = new Game();
         game.board.cells[1][3] = 'RW';
         game.board.cells[1][2] = '';
         game.board.cells[1][1] = 'WRWR';
@@ -85,13 +94,15 @@ describe('Game engine', () => {
         }, { name: 'Error', message: 'Bad targetted distance' });
     })
     it('can goes (splited T4 on T1)', () => {
+        const game = new Game();
         game.board.cells[0][1] = 'R';
-        game.board.cells[0][2] = 'WRWR';
+        game.board.cells[0][2] = 'WRWR';;
         assert.doesNotThrow(() => {
             game.play(game.players['W'], {type: 'move', from: [0,2], to: [0,1], nbPieces: 4})
         }, { name: 'Error', message: 'Bad targetted distance' });
     })
     it('can goes (T3 on T2)', () => {
+        const game = new Game();
         game.board.cells[2][1] = 'WRW';
         game.board.cells[2][2] = '';
         game.board.cells[2][3] = 'WR';
@@ -100,6 +111,7 @@ describe('Game engine', () => {
         }, { name: 'Error', message: 'Bad targetted distance' });
     })
     it('must not have piece between', () => {
+        const game = new Game();
         game.board.cells[1][1] = 'WWR';
         game.board.cells[3][3] = 'R';
         game.board.cells[4][4] = 'WRW';
@@ -126,13 +138,8 @@ describe('Game engine', () => {
         }, { name: 'Error', message: 'Unempty cells between start and end' });
     })
 
-    // End Game
-    game.players['W'].score = 0;
-    game.players['R'].score = 0;
-    game.players['W'].backpieces = 18;
-    game.players['R'].backpieces = 18;
-    //Win
     it('checks W win', () => {
+        const game = new Game();
         game.board.cells[0][0] = 'RWRW';
         game.board.cells[0][4] = 'RWRW';
         game.play(game.players['W'], {type: 'move', from: [0,0], to: [0,4], nbPieces: 4});
@@ -143,11 +150,12 @@ describe('Game engine', () => {
     })
     //Pass
     it('checks W pass', () => {
-        /* Le joueur clique sur "Passer son tour" : */
+        const game = new Game();
         assert(game.players['W'].pass === true);
     })
     //draw (2 forms)
     it('checks draw by 2 passes', () => {
+        const game = new Game();
         game.players['W'].winner = false;
         game.players['R'].winner = false;
         game.players['W'].score = 1;
